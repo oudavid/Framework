1add47dd0478874f5afa3209ef05ddf6a61e5f80
oudavid <ewhsoud@yahoo.com>
2015-05-11 13:31:18 -0700

    Add Challenge Activity
    Add Challenge Activity

---
+++ app/src/main/java/com/aim/framework/ChallengeActivityFragment.java
@@ -0,0 +1,48 @@
+package com.aim.framework;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ChallengeActivityFragment extends Fragment {
+    DraggableAndMultiSelectableRecyclerView mListView;
+
+    public ChallengeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_challenge, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mListView = (DraggableAndMultiSelectableRecyclerView) view.findViewById(R.id.listView);
+        mListView.setAdapter(new DraggableAndMultiSelectableRecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 10;
+            }
+        });
+    }
+}
